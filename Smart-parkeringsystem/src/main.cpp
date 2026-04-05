baafad59890953bdf556f3da1ff71326108a1bcb
Yousif Al-Anbari <slaiver@live.se>
2026-04-05 20:30:24 +0200

    PIN-DEFINITIONER

--- Smart-parkeringsystem/src/main.cpp
+++ Smart-parkeringsystem/src/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * SMART PARKING SYSTEM - PROFESSIONELL VERSION (NON-BLOCKING)
+ * Hårdvara: ESP32 DevKitC V4 (38-pin), 2x RFID, 2x Servo, 2x LCD, 4x HC-SR04, 1x Buzzer
+ * Databas: Redis via Wi-Fi (med LittleFS Offline-stöd)
+ */
+
+#include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h>
@@ -6,3 +13,26 @@
 #include <WiFi.h>
 #include <LittleFS.h>
 #include <Redis.h>
+
+// =======================================================
+// 2. PIN-DEFINITIONER
+// =======================================================
+#define BUZZER_PIN 2
+
+#define RST_PIN 27
+#define SS_IN_PIN 5
+#define SS_OUT_PIN 4
+
+#define SERVO_IN_PIN 13
+#define SERVO_OUT_PIN 14
+
+#define TRIG_IN_BEFORE 32
+#define ECHO_IN_BEFORE 34
+#define TRIG_IN_AFTER 33
+#define ECHO_IN_AFTER 35
+
+#define TRIG_OUT_BEFORE 25
+#define ECHO_OUT_BEFORE 36
+#define TRIG_OUT_AFTER 26
+#define ECHO_OUT_AFTER 39
+
